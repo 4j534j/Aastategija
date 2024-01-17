f7bbc5d8ffbbe1e989ce74b426c138f9a387417b
4j534j <4j534j@gmail.com>
2024-01-17 19:21:46 +0200

    fic some coloerer

--- src/js/schedule.js
+++ src/js/schedule.js
@@ -1,8 +1,6 @@
 function daychange(day, curday) {
     curday = document.getElementById(curday);
     curday.style.display = 'none';
-
-    
     alttag = day.getAttribute('alt');
     chosenday = document.getElementById(alttag);
     chosenday.style.display = 'block';
