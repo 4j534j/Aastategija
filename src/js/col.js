1612d3f2e3244526582a3e2260d23298612a48d0
4j534j <4j534j@gmail.com>
2024-01-18 02:48:09 +0200

    colchange not yet

---
+++ src/js/col.js
@@ -0,0 +1,8 @@
+function colchange(){
+    
+    const root = document.documentElement;
+    root.style.setProperty('--black', 'white');
+    root.style.setProperty('--gray', '#F5F7FA');
+    root.style.setProperty('--about-text', '#3D3D3F')
+
+}
