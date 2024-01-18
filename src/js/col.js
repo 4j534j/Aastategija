b175f984fb54fa079132d448ebb1fcf597ee57b4
4j534j <4j534j@gmail.com>
2024-01-18 03:35:06 +0200

    light mode progress

--- src/js/col.js
+++ src/js/col.js
@@ -4,5 +4,10 @@ function colchange(){
     root.style.setProperty('--black', 'white');
     root.style.setProperty('--gray', '#F5F7FA');
     root.style.setProperty('--about-text', '#3D3D3F')
+    root.style.setProperty('--about-trophy-text', '#1E1F21')
+    root.style.setProperty('--about-guide-text', '#1E1F21')
+    root.style.setProperty('--light-blue', '#1E1F21')
+    root.style.setProperty('--EKKSL-beige', '#3D3D3F')
+
 
 }
